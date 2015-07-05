b3b8398e80b675c1c7a86d25ba60463cc65de13d
Manitra Andriamitondra <manitra@manitra.net>
2015-07-05 22:15:28 +0200

    Adding install file with user, ssh, vm configs

---
+++ install.sh
@@ -0,0 +1,20 @@
+# Create user
+useradd manitra
+useradd manitra sudo
+
+# Configure sshd 
+echo "AllowGroups sshusers" >> /etc/ssh/sshd_config
+groupadd sshusers
+useradd manitra sshusers
+
+# Configure ufw to allow ssh, http and https only
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw enable
+
+# Install KVM (https://help.ubuntu.com/community/KVM/Installation#Installation_of_KVM)
+apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-viewer
+
+echo "Log off and log one again"
+
