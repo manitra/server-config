56b3fadf7691d4624b0b83f9da81a0c70d046959
Manitra Andriamitondra <manitra@manitra.net>
2015-07-30 21:13:29 +0700

    Make the backup script loop until it's finished

--- home01/backup.sh
+++ home01/backup.sh
@@ -1,3 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 
-rsync -avx ~/backup/photos manitra@web01.manitra.net:backup
+RC=1 
+while [[ RC -ne 0 ]]
+do
+	rsync -avx ~/backup/photos manitra@web01.manitra.net:backup
+	RC=$?
+	sleep 5
+done
