86dda554599c4100b5c2556ee32ceb59ff0f963a
Manitra Andriamitondra <manitra@manitra.net>
2015-08-01 16:14:19 +0700

    Parallelize rsync per YEAR folder
    Vietnam to France takes 300+ ms so tcp is extremely limited
    Parralelizing allows me to use all my 10mbps of upload bandwith

--- home01/backup.sh
+++ home01/backup.sh
@@ -3,7 +3,7 @@
 RC=1 
 while [[ RC -ne 0 ]]
 do
-	rsync -avx ~/backup/photos manitra@web01.manitra.net:backup
+	ls ~/backup/photos | parallel -v -j10 -I '$' --line-buffer rsync -avx '/home/manitra/backup/photos/$' 'manitra@web01.manitra.net:backup/photos'
 	RC=$?
 	sleep 5
 done
