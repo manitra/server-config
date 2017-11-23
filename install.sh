b447441950f722aef75e05872257e6b41c3cb4d4
Manitra Andriamitondra <manitra@manitra.net>
2017-11-23 15:42:27 +0300

    Add FTP gateway to vm03 for VestaCP

--- install.sh
+++ install.sh
@@ -75,11 +75,11 @@ echo "192.168.122.103 vm03.web01.manitra.net vm03" >> /etc/hosts
 # - vm01 is the main web server
 # - vm03 is BitTorrent Sync administration on port 8888
 # then we save it to /etc/iptables.rules that we load on startup
-PUB_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 VLAN_RANGE="192.168.122.0/24"
-iptables -t nat    -I PREROUTING -p tcp -d $PUB_IP --dport 80 -j DNAT --to-destination 192.168.122.101:80
-iptables -t nat    -I PREROUTING -p tcp -d $PUB_IP --dport 8002 -j DNAT --to-destination 192.168.122.102:8080
-iptables -t nat    -I PREROUTING -p tcp -d $PUB_IP --dport 8003 -j DNAT --to-destination 192.168.122.103:8888
+iptables -t nat    -I PREROUTING -p tcp -i eth0 --dport 80          -j DNAT --to-destination 192.168.122.101:80
+iptables -t nat    -I PREROUTING -p tcp -i eth0 --dport 443         -j DNAT --to-destination 192.168.122.101:443
+iptables -t nat    -I PREROUTING -p tcp -i eth0 --dport 21          -j DNAT --to-destination 192.168.122.103:21
+iptables -t nat    -I PREROUTING -p tcp -i eth0 --dport 12000:12100 -j DNAT --to-destination 192.168.122.103:12000-12100
 iptables -t filter -I FORWARD -m state -d $VLAN_RANGE --state NEW,RELATED,ESTABLISHED -j ACCEPT
 iptables-save > /etc/iptables.rules
 cp iptablesload /etc/network/if-pre-up.d/
